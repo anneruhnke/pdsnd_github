481053e6fab366d7d7ee82846d93854f30538429
Anne Ruhnke <anne_ruhnke@t-online.de>
2020-11-23 07:44:46 +0100

    Docs: Rename variable for most recent birth year

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -182,8 +182,8 @@ def user_stats(df):
         birth_year_min = df['Birth Year'].min()
         print('The earliest birth year:', birth_year_min)
 
-        max_birth_year = df['Birth Year'].max()
-        print('The most recent birth year:', max_birth_year)
+        birth_year_max = df['Birth Year'].max()
+        print('The most recent birth year:', birth_year_max)
     except:
         print('Gender data is not available for this city.')
 
